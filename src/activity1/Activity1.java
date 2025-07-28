1a791ca558013eb0cd1d374b8021becc95eded30
Nacorda-G <gracelynacz54@gmail.com>
2025-07-28 14:31:11 +0800

    second time

---
+++ src/activity1/Activity1.java
@@ -0,0 +1,42 @@
+
+package activity1;
+import java.util.Scanner;
+
+public class Activity1 {
+
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String name;
+        int subjects;
+        float ave = 0;
+        
+        
+        System.out.print("Enter students name: ");
+        name = sc.nextLine();
+        
+        System.out.print("Enter number of subjects: ");
+        subjects = sc.nextInt();
+        
+            for(int x = 0; x < subjects; x++){
+                float grade;
+                
+                System.out.print("Enter grade for Sub: " +(x + 1)+ ":");
+                    grade = sc.nextFloat();
+                    ave += grade / subjects;
+            }
+           
+        System.out.println("Average: "+ave);
+       
+        if (ave <=100 && ave >= 75){
+            System.out.println("Remarks: Passed");
+        }else{
+            System.out.println("Remarks: Failed");
+        }
+        
+        
+    }
+}
+    
+
