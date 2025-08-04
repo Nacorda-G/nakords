06c038f8abf4ce9331bbee27e704bad5def92ed4
jamesoliverbastismo <jamesoliverbastismo35@gmail.com>
2025-08-04 15:16:43 +0800

    update

---
+++ src/activity1/Main.java
@@ -0,0 +1,55 @@
+package appsecb;
+
+import java.util.Scanner;
+import banking.bankingAPP;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int attemp = 0;
+        
+        System.out.println("HELLO WELCOME TO THE SYSTEM!");
+        System.out.println("What do you feel doing today? ");
+        System.out.println("1. Banking");
+        System.out.println("2. Doctors Appointment");
+        System.out.println("3. Shopping");
+        
+        System.out.print("Enter choice: ");
+        int choice = sc.nextInt();
+        
+        switch(choice){
+            case 1:
+                while(attemp != 3){
+                    bankingAPP bc = new bankingAPP();
+
+                    System.out.println("Enter your Account No: ");
+                    int accountNo = sc.nextInt();
+
+                    System.out.println("Enter your Pin: ");
+                    int pin = sc.nextInt();
+
+                        if(bc.verifyAccount(accountNo, pin)){
+                            System.out.println("LOGIN SUCCESS");
+                        }else{
+                            System.out.println("INVALID ACCOUNT!");
+                            attemp++;
+                        }
+                }
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            default:
+                System.out.println("Invalid Selection!");
+        
+        }
+            
+    }
+    
+}
